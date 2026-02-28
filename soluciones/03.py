3314c8216e3df96a240399b6ea67465448749178
julisapuyes588-cell <julisapuyes588@gmail.com>
2026-02-28 02:27:34 +0000

    solucionej3

---
+++ soluciones/03.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+RUTA_ARCHIVO='data/personas.csv'
+
+datos = pd.read_csv(RUTA_ARCHIVO)
+## print(datos.head())
+
+def decifrar_palabra (palabra_cifrada):
+    import codecs
+    nueva_palabra =codecs.encode(palabra_cifrada,'rot 13')
+    return nueva_palabra
+
+datos ['nombre'] = datos['nombre_cifrado'].apply(decifrar_palabra)
+condicion = datos ['nombre']=="Juan"
+print ("Juan aparece:",datos[condicion].shape[0])
